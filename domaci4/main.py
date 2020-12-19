d28476d7ca5bd179d611d5725e2697c558d2b84f
imafikus <aleksatesicteske@gmail.com>
2020-12-19 16:25:45 +0100

    Add C calculation

--- domaci4/main.py
+++ domaci4/main.py
@@ -10,6 +10,33 @@ def correct_if_needed(Q, R):
                 Q[i][d] = -Q[i][d]
     return Q, R
 
+def calc_C(T):
+    c1 = 1 * np.linalg.det(np.array([
+        [T[0][1], T[0][2], T[0][3]],
+        [T[1][1], T[1][2], T[1][3]],
+        [T[2][1], T[2][2], T[2][3]],
+    ]))
+
+    c2 = -1 * np.linalg.det(np.array([
+        [T[0][0], T[0][2], T[0][3]],
+        [T[1][0], T[1][2], T[1][3]],
+        [T[2][0], T[2][2], T[2][3]],
+    ]))
+
+    c3 = 1 * np.linalg.det(np.array([
+        [T[0][0], T[0][1], T[0][3]],
+        [T[1][0], T[1][1], T[1][3]],
+        [T[2][0], T[2][1], T[2][3]],
+    ]))
+
+    c4 = -1 * np.linalg.det(np.array([
+        [T[0][0], T[0][1], T[0][2]],
+        [T[1][0], T[1][1], T[1][2]],
+        [T[2][0], T[2][1], T[2][2]],
+    ]))
+
+    return np.array([[c1, c2, c3]]) / c4
+
 def main():
     n = 11 # 121 / 2017
     T = np.array([
@@ -28,7 +55,6 @@ def main():
         T0 = -T0
 
     T0_inv = np.linalg.inv(T0)
-    print(T0_inv)
 
     Q, R = np.linalg.qr(T0_inv)
     Q, R = correct_if_needed(Q, R)
@@ -39,10 +65,16 @@ def main():
 
     A = np.linalg.inv(Q)
 
+    C = calc_C(T)
+
     print('=== K ===\n', K)
     print()
     
     print('=== A ===\n', A)
+    print()
+
+        
+    print('=== C ===\n', C)
     print()
 
 if __name__ == "__main__":
